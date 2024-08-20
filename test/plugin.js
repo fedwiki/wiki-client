d20bbc4876a540c5c580e095f2cb9e83f66c82d8
Paul Rodwell <paul.rodwell@btinternet.com>
2024-08-20 12:19:28 +0100

    update plugin tests
    - remove tests of ajax plugin loading (obsolete)
    - add 'npm test' script, replaces 'grunt mochaTest'

--- test/plugin.js
+++ test/plugin.js
@@ -16,29 +16,29 @@ describe('plugin', function() {
   before(function() {
     $page = $('<div id="plugin" />');
     $page.appendTo('body');
-    return sinon.spy(jQuery, 'ajax');
+    // return sinon.spy(jQuery, 'ajax');
   });
 
   after(function() {
-    jQuery.ajax.restore();
-    return $page.empty();
+    // jQuery.ajax.restore();
+    $page.empty();
   });
 
   it('should have default reference type', () => expect(window.plugins).to.have.property('reference'));
 
-  it('should fetch a plugin script from the right location', function() {
-    plugin.get('activity');
-    expect(jQuery.ajax.calledOnce).to.be(true);
-    return expect(jQuery.ajax.args[0][0].url).to.be('/plugins/activity/activity.js');
-  });
+  // it('should fetch a plugin script from the right location', function() {
+  //   plugin.get('activity');
+  //   expect(jQuery.ajax.calledOnce).to.be(true);
+  //   return expect(jQuery.ajax.args[0][0].url).to.be('/plugins/activity/activity.js');
+  // });
 
-  return it.skip('should render a plugin', function() {
+  it.skip('should render a plugin', function() {
     const item = {
       type: 'paragraph',
       text: 'blah [[Link]] asdf'
     };
     plugin.do($('#plugin'), item);
-    return expect($('#plugin').html()).to
+    expect($('#plugin').html()).to
       .be('<p>blah <a class="internal" href="/link.html" data-page-name="link" title="view">Link</a> asdf</p>');
   });
 });
