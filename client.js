0f4740a6752e0cfda54f5ddf5674aef97395c7b5
SamAxe <1377880+SamAxe@users.noreply.github.com>
2024-09-06 16:31:42 -0700

    Convert CJS modules to ESM style
    Made esbuild _compile_, but certainly not in working form
    
    Modules with data were converted to classes.

--- client.js
+++
@@ -1,8 +0,0 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
-window.name = window.location.host;
-
-window.wiki = require('./lib/wiki');
-require('./lib/legacy');
-require('./lib/bind');
-require('./lib/plugins');
---
+++ client.mjs
@@ -0,0 +1,9 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
+window.name = window.location.host;
+
+import * as wiki from './lib/wiki.mjs';
+window.wiki = wiki;
+import './lib/legacy.mjs';
+import './lib/bind.mjs';
+import './lib/plugins.mjs';
