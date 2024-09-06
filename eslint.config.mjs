6534f6293bd45e05ed32cd6ddfeb9133ffe026b0
SamAxe <1377880+SamAxe@users.noreply.github.com>
2024-09-05 18:33:22 -0700

    several eslint warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,10 +4,12 @@ import pluginJs from "@eslint/js";
 
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {ignores: ["outdir/*", "test/*", "scripts/*"]},
   {languageOptions: { 
     globals: {
       ...globals.browser,
       ...globals.jquery,
+      ...globals.mocha,
     }
   }
   },
