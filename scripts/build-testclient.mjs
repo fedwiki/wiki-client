f9caad526706db208a653992d6eac5224681de1e
Paul Rodwell <paul.rodwell@btinternet.com>
2024-09-11 11:03:04 +0100

    add banner to test client

--- scripts/build-testclient.mjs
+++ scripts/build-testclient.mjs
@@ -1,8 +1,17 @@
 import * as esbuild from 'esbuild'
+import fs from 'node:fs/promises'
+import packJSON from "../package.json" with { type: "json"};
+
+const version = packJSON.version
+const now = new Date()
 
 let results = await esbuild.build({
   entryPoints: ['testclient.js'],
   bundle: true,
+  banner: {
+    js: `/* wiki-client (test) - ${version} - ${now.toUTCString()} */`},
+  minify: true,
+  sourcemap: true,
   logLevel: 'info',
   outfile: 'client/test/testclient.js'
 })
