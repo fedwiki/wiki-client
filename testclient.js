0ee0dda2569404d813ba19b29387c6cc87c8d149
Paul Rodwell <paul.rodwell@btinternet.com>
2024-11-02 17:09:38 +0000

    DS102 in testclient

--- testclient.js
+++ testclient.js
@@ -1,10 +1,3 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
- */
 mocha.setup('bdd');
 
 window.wiki = require('./lib/wiki');
@@ -29,6 +22,5 @@ require('./test/random');
 
 $(function() {
   $('<hr><h2> Testing artifacts:</h2>').appendTo('body');
-  return mocha.run();
+  mocha.run();
 });
-
