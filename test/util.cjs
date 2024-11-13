2745876e81cc5cb2913dc33b8d836b6ddecd06f8
SamAxe <1377880+SamAxe@users.noreply.github.com>
2024-11-13 07:29:40 -0800

    Convert util.cjs to util.mjs

--- test/util.cjs
+++ test/util.cjs
@@ -5,7 +5,7 @@
  * DS102: Remove unnecessary code created because of implicit returns
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
  */
-const util = require('../lib/util.cjs');
+const {formatTime, formatDate} = require('../lib/util.mjs');
 const expect = require('expect.js');
 
 const timezoneOffset = () => (new Date(1333843344000)).getTimezoneOffset() * 60;
@@ -13,15 +13,15 @@ const timezoneOffset = () => (new Date(1333843344000)).getTimezoneOffset() * 60;
 describe('util', function() {
 
   it('should format unix time', function() {
-    const s = util.formatTime(1333843344 + timezoneOffset());
+    const s = formatTime(1333843344 + timezoneOffset());
     return expect(s).to.be('12:02 AM<br>8 Apr 2012');
   });
   it('should format javascript time', function() {
-    const s = util.formatTime(1333843344000 + (timezoneOffset() * 1000));
+    const s = formatTime(1333843344000 + (timezoneOffset() * 1000));
     return expect(s).to.be('12:02 AM<br>8 Apr 2012');
   });
   return it('should format revision date', function() {
-    const s = util.formatDate(1333843344000 + (timezoneOffset() * 1000));
+    const s = formatDate(1333843344000 + (timezoneOffset() * 1000));
     return expect(s).to.be('Sun Apr 8, 2012<br>12:02:24 AM');
   });
 });
