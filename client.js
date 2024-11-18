485635f1516600e07047f37c2a2e794030050693
Paul Rodwell <paul.rodwell@btinternet.com>
2024-11-18 12:35:45 +0000

    formatting with prettier

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 // TODO: This file was created by bulk-decaffeinate.
 // Sanity-check the conversion and remove this comment.
-window.name = window.location.host;
+window.name = window.location.host
 
-window.wiki = require('./lib/wiki');
-require('./lib/legacy');
-require('./lib/bind');
-require('./lib/plugins');
+window.wiki = require('./lib/wiki')
+require('./lib/legacy')
+require('./lib/bind')
+require('./lib/plugins')
